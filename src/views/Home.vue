120cba049b300fffc60ad81672cfe830dd8b4458
Mahyar Behzadi <behzadimahyar85@gmail.com>
2021-09-10 13:02:05 +0430

    Getting data from openweathermap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="home">
     <ul>
-      <li v-for="city in cities" :key="city.id">{{ city.name }}</li>
+      <!-- <li v-for="city in cities" :key="city.id">{{ city.name }}</li> -->
+      <input type="search" v-model="key" placeholder="City name" @change="searchCity(key)" />
+      <p>{{ data }}</p>
     </ul>
   </div>
 </template>
 
 <script>
-// import CitiesData from '@/data/city.list.min.json'
-import CitiesData from '@/data/city.list.json'
+import axios from 'axios'
+import CitiesData from '@/data/city.list.min.json'
 
 export default {
   name: 'Home',
   data() {
     return {
+      search: "",
+      data: "d",
       cities: CitiesData,
     };
   },
+  methods: {
+    searchCity(key) {
+      axios.get('https://api.openweathermap.org/data/2.5/weather?id=' + key + '&appid=2d423db4a8520eb6e988b9c97ccfebc6').then(response => {
+        console.log(response)
+      });
+    }
+  }
 }
 </script>
